c4c36537277c512bffd1ff9c84df5d07d62a4dd7
Dipen Boro <borod9200@gmail.com>
2026-03-16 07:54:57 +0000

    added bouch of configs

---
+++ src/config/env.validation.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+
+export const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'prod' , 'test' ]).default('prod'),
+    PORT: z.coerce.number().positive().default(8001),
+    DATABASE_URL: z.string().url(),
+})
+
+export type EnvVars = z.infer<typeof envSchema>;
+
+export function validate(config: Record<string, unknown>) {
+    const parsed = envSchema.safeParse(config);
+
+    if (!parsed.success) {
+        console.error('❌ Invalid environment variables:', parsed.error.flatten().fieldErrors);
+        throw new Error('Invalid environment variables');
+    }
+    return parsed.data;
+
+}
