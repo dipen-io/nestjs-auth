5d426e8e9b05d36cd5b4247799eac943052f0762
Dipen Boro <borod9200@gmail.com>
2026-03-16 14:58:14 +0000

    migration done

---
+++ drizzle/0000_mute_the_order.sql
@@ -0,0 +1,11 @@
+CREATE TYPE "public"."role" AS ENUM('user', 'admin');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"fullname" varchar(255) NOT NULL,
+	"password" varchar(245) NOT NULL,
+	"role" "role" DEFAULT 'user',
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
