2bcf714edb5a74334303210128632a89b4229d57
Dipen Boro <borod9200@gmail.com>
2026-03-16 11:19:38 +0000

    controller for login & register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpCode, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
+    @Post('signup')
+    @HttpCode(HttpStatus.CREATED)
+    async signup(@Body() createAuthDto: CreateAuthDto) {
+        return this.authService.register(createAuthDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
+    @Post('login')
+    async login(@Body() dto: LoginDto) {
+        const user = await this.authService.validateUser(dto.email, dto.password);
+        if (!user) {
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+        //NOTE: we will return an jwt here 
+        return { message: 'Login Successfully', user }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.authService.findOne(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+        return this.authService.update(+id, updateAuthDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.authService.remove(+id);
+    }
 }
