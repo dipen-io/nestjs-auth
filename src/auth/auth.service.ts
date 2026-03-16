50cd326be464fe97bcdbb91a6897984607caca75
Dipen Boro <borod9200@gmail.com>
2026-03-16 11:20:10 +0000

    auth service added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,63 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import * as schema from '../database/schema/';
+import { DATABASE_TOKEN } from 'src/database/database.provider';
+import * as schema from '../database/schema/'
+import * as argon2 from 'argon2';
+import type { DB } from 'src/database/types';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class AuthService {
     constructor(
-        @Inject('DRIZZLE_CONNECRTION')
-        private db: NodePgDatabase< typeof schema >
+        @Inject(DATABASE_TOKEN)
+        private db: DB
     ){}
-    register(createAuthDto: CreateAuthDto) {
-        return 'This action adds a new auth';
+    async register(createAuthDto: CreateAuthDto) {
+        // Hash the Password 
+        const hashedPassword = await argon2.hash(createAuthDto.password);
+
+        try {
+            // Insert User into db  
+            const [newUser] = await this.db.insert(schema.users).values({
+                email: createAuthDto.email.toLowerCase(),
+                password: hashedPassword,
+                fullname: createAuthDto.fullname
+            })
+                .returning(
+                    {
+                        id: schema.users.id,
+                        email: schema.users.email,
+                        fullname: schema.users.fullname,
+                        createdAt: schema.users.createdAt,
+
+                    }
+                )
+            return newUser;
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Email already exist!');
+            }
+            throw new InternalServerErrorException('Registration Failed');
+        }
+    }
+
+    async validateUser(email: string, pass: string): Promise<any>{
+        // find user by email
+        const [user] = await this.db
+        .select()
+        .from(schema.users)
+        .where(eq(schema.users.email, email.toLowerCase()));
+
+        if (user) {
+            // compare password
+            const isPassMatch = await argon2.verify(user.password, pass);
+            if (isPassMatch) {
+                const {password, ...result} = user;
+                return result;
+            }
+        }
+        return null
     }
 
     findAll() {
